cf3f8cc57994da0d0d4e53624a4bfdc9e54c2055
sneha chauhan <nchauhansneha@gmail.com>
2024-07-29 12:42:49 +0530

    add new pro

--- public/js/map.js
+++ public/js/map.js
@@ -1,20 +1,24 @@
-
 let MAP_TOKEN = mapToken;
 console.log(mapToken)
 mapboxgl.accessToken = mapToken
-{/* // TO MAKE THE MAP APPEAR YOU MUST */ }
-{/* // ADD YOUR ACCESS TOKEN FROM */ }
-{/* // https://account.mapbox.com */ }
+{/* // TO MAKE THE MAP APPEAR YOU MUST / }
+{/ // ADD YOUR ACCESS TOKEN FROM / }
+{/ // https://account.mapbox.com/ */ }
 const map = new mapboxgl.Map({
     container: 'map',
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/streets-v12', //style URL
-    center: [72.68333000, 23.21667000], //starting position [longti,lat]  ki postion ae map khule
-    zoom: 9 //starting Zoom
+    center:listing.geometry.coordinates, //starting position [longti,lat]  ki postion ae map khule
+    zoom: 10//starting Zoom
 });
-console.log(coodinates);
+console.log(coordinates);//print corrdinates to console
 
-    // Create a default Marker and add it to the map.
-    // const marker1 = new mapboxgl.Marker()
-    //     .setLngLat([coodinates]) //Model_listing.js->Geomatery.Coordinates ave
-    //     .addTo(map);
+mapboxgl.accessToken=window.mapToken;
+
+// Create a default Marker and add it to the map.
+const marker = new mapboxgl.Marker({ color: "red" })
+    .setLngLat( listing.geometry.coordinates) //Model_listing.js->Geomatery.Coordinates ave
+    .setPopup(new mapboxgl.Popup({ offset: 20 }).setHTML(
+        `<h4>${listing.title}</h4> <p>Exact Location will be provided after booking</p>`))
+    .addTo(map);
+
